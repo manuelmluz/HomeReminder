4ecabf3b8e910994e5beac430b2337199b1e7965
MANUEL <MirandaMendesDaLuzM@cardiff.ac.uk>
2025-10-18 16:39:42 +0100

    initial commit

---
+++ HomeReminder/main/models.py
@@ -0,0 +1,11 @@
+
+from django.contrib.auth.models import AbstractUser
+# from django.db import models
+
+# Create your models here.
+class User(AbstractUser):
+    # abstract user already has name password username type shit
+    # can still add more if needed
+
+    #listings field??
+    pass
